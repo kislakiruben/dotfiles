a7b9e4cf3c9bf6773287c599f9b1511049fb9e55
Ruben Kislaki <ruben@kislaki.net>
2025-01-03 11:43:53 +0200

    refactor and reformat how certain plugins are inited

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,6 +2,21 @@ return {
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- Useful status updates for LSP
+  {
+    'j-hui/fidget.nvim',
+    config = true,
+  },
+
+  -- Autocomplete
+  'hrsh7th/vim-vsnip-integ',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-vsnip',
+  'hrsh7th/vim-vsnip',
+
   -- LSPs
   'neovim/nvim-lspconfig',
   'williamboman/mason.nvim',
@@ -11,6 +26,7 @@ return {
   'github/copilot.vim',
 
   -- Appearance
+  'EdenEast/nightfox.nvim',
   {
     'freddiehaddad/feline.nvim',
     dependencies = {
@@ -24,19 +40,19 @@ return {
   -- used only by feline (for now)
   'lewis6991/gitsigns.nvim',
   {
-    "NeogitOrg/neogit",
+    'NeogitOrg/neogit',
     dependencies = {
-      "nvim-lua/plenary.nvim",  -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
+      'nvim-lua/plenary.nvim',  -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
 
       -- Only one of these is needed.
-      "nvim-telescope/telescope.nvim", -- optional
+      'nvim-telescope/telescope.nvim', -- optional
     },
     config = true
   },
   {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
   }
 }
