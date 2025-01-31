ffa1ba102fbfea13458dbe29662ba3d7e2c22ba4
Ruben Kislaki <ruben@kislaki.net>
2025-01-31 12:07:01 +0200

    tweak mini.nvim config

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,17 +2,15 @@
 return {
   'echasnovski/mini.nvim',
   config = function()
-    require('mini.align').setup()
     require('mini.comment').setup()
     require('mini.completion').setup()
     require('mini.fuzzy').setup()
-    require('mini.jump').setup()
     require('mini.jump2d').setup()
-    require('mini.misc').setup()
-    require('mini.pairs').setup()
-    -- require('mini.statusline').setup()
     require('mini.surround').setup()
-    require('mini.trailspace').setup()
-    require('mini.ai').setup()
+    require('mini.indentscope').setup({
+      draw = {
+        animation = require('mini.indentscope').gen_animation.none()
+      }
+    })
   end
- }
+}
