cea1deac556b9c0fe9c758c149e07000a7cf3cbc
Ruben Kislaki <ruben@kislaki.net>
2025-02-13 01:11:16 +0200

    installed snacks

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  priority = 1000,
+  --@type snacks.Config
+  opts = {
+    input = { enabled = true },
+    indent = { enabled = true },
+    notifier = { enabled = true },
+    scope = { enabled = true },
+    words = { enabled = true },
+  },
+}
