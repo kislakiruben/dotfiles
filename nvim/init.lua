467ac54099e39bb904e228e2356d5c73a42ea4c4
Ruben Kislaki <ruben@kislaki.net>
2025-02-13 15:15:08 +0200

    restored feline

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
 require("config.lazy")
 require("ui.tabby")
 require("ui.lsp_progress")
+require("ui.feline")
---
+++ nvim/lua/plugins/statusline.lua
@@ -0,0 +1,6 @@
+return {
+  "freddiehaddad/feline.nvim",
+  dependencies = {
+    "kyazdani42/nvim-web-devicons",
+  },
+}
