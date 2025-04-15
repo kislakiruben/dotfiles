8ce72968d7a7ba6f5f720e557ade06efc58ae253
Ruben Kislaki <ruben@kislaki.net>
2025-04-15 10:56:05 +0300

    attempt to fix cmp

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,61 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",           -- source for text in buffer
+    "hrsh7th/cmp-path",             -- source for file system paths
+    "hrsh7th/cmp-cmdline",
+    "L3MON4D3/LuaSnip",             -- snippet engine
+    "rafamadriz/friendly-snippets", -- useful snippets
+    "saadparwaiz1/cmp_luasnip",     -- autocompletion
+    "onsails/lspkind.nvim",         -- vs-code like pictograms
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+
+    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+        confirmation = { completeopt = 'menu,menuone,noinsert' },
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          mawidth = 50,
+          ellpsis_char = "...",
+        }),
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = "cmdline" },
+      },
+    })
+
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+  end
+}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,63 +1,39 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          -- "ast_grep", -- js, ts, jsx, json, html
-          "cssls",
-          "gopls",
-          "html",
-          "jsonls",
-          "lua_ls",
-          "sorbet", -- ruby
-          "tailwindcss",
-          "vtsls",  -- js, ts
-        }
-      })
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
-        ["vtsls"] = function()
-          require("lspconfig").vtsls.setup({
-            root_dir = require("lspconfig").util.root_pattern(
-              ".git",
-              "yarn.lock",
-              "package-lock.json",
-              "pnpm-lock.yaml"
-            ),
-          })
-        end,
-      })
-    end
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
   },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/vim-vsnip-integ",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-vsnip",
-    },
-    config = function()
-      require("cmp").setup({
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-          { name = "cmdline" },
-          { name = "vsnip" },
-        },
-      })
-    end
-  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        -- "ast_grep", -- js, ts, jsx, json, html
+        "cssls",
+        "gopls",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "sorbet",   -- ruby
+        "tailwindcss",
+        "vtsls",    -- js, ts
+      }
+    })
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        require("lspconfig")[server_name].setup({})
+      end,
+      ["vtsls"] = function()
+        require("lspconfig").vtsls.setup({
+          root_dir = require("lspconfig").util.root_pattern(
+            ".git",
+            "yarn.lock",
+            "package-lock.json",
+            "pnpm-lock.yaml"
+          ),
+        })
+      end,
+    })
+  end
 }
