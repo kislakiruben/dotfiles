ad99af1b644ab35fd010b09ba0f465e0c66fea8a
Ruben Kislaki <ruben@kislaki.net>
2026-02-04 22:21:57 +0100

    shortcut key to notification history

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -6,9 +6,17 @@ return {
   opts = {
     input = { enabled = true },
     indent = { enabled = true },
-    notify = { enabled = true },
+    notify = { enabled = true, style = "fancy" },
     notifier = { enabled = true },
     scope = { enabled = true },
     words = { enabled = true },
+    styles = {
+      notification_history = {
+        width = 0.9,
+      },
+    },
+  },
+  keys = {
+    { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
   },
 }
