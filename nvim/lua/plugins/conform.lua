fbe5b8c58a2c0e1a4669331df32f06126d7c13dc
Ruben Kislaki <ruben@kislaki.net>
2026-02-04 22:17:43 +0100

    attempting to fix prettier

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,17 +1,22 @@
 return {
   "stevearc/conform.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
       javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
       typescript = { "prettierd" },
+      typescriptreact = { "prettier" },
       json = { "prettierd" },
       yaml = { "prettierd" },
       markdown = { "prettierd" },
       html = { "prettierd" },
+      css = { "prettierd" },
     },
     format_on_save = {
-      timeout_ms = 500,
+      timeout_ms = 1000,
       lsp_format = "fallback",
     },
   },
